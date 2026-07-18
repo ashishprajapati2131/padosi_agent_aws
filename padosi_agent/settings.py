99c55dfdef7e82698de4d25308d7197c792d8078
Anurag <anurag07.le@gmail.com>
2026-07-18 15:36:38 +0530

    Remove default database fallbacks from settings.py

--- padosi_agent/settings.py
+++ padosi_agent/settings.py
@@ -107,11 +107,11 @@ WSGI_APPLICATION = 'padosi_agent.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'padosiagent'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
